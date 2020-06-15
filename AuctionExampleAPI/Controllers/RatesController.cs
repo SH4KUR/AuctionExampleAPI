3595d561b8b0eaa94e94b5ac6ca851dd2dcd8097
Dmitriy Lopatiev <dmitriy.lopatiev@gmail.com>
2020-06-15 21:11:43 +0300

    Add Rates Controller

--- AuctionExampleAPI/Controllers/RatesController.cs
+++ AuctionExampleAPI/Controllers/RatesController.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AuctionExampleAPI.Data;
-using AuctionExampleAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using AuctionExampleAPI.Data;
+using AuctionExampleAPI.Models;
 
 namespace AuctionExampleAPI.Controllers
 {
-    [ApiController]
     [Route("api/[controller]")]
+    [ApiController]
     public class RatesController : ControllerBase
     {
         private readonly AuctionExampleContext _context;
@@ -20,27 +21,86 @@ namespace AuctionExampleAPI.Controllers
             _context = context;
         }
 
-        // GET: api/rates
+        // GET: api/Rates
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Rate>>> Get()
+        public async Task<ActionResult<IEnumerable<Rate>>> GetRate()
         {
-            return await _context.Rate
-                .Include(i => i.Item)
-                .ToListAsync();
+            return await _context.Rate.ToListAsync();
         }
 
-        // GET: api/rates/1
+        // GET: api/Rates/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Rate>> Get(int id)
+        public async Task<ActionResult<Rate>> GetRate(int id)
         {
-            var rate = await _context.Rate
-                .Include(i => i.Item)
-                .FirstOrDefaultAsync(i => i.RateId == id);
+            var rate = await _context.Rate.FindAsync(id);
 
             if (rate == null)
+            {
                 return NotFound();
+            }
 
             return rate;
+        }
+
+        // PUT: api/Rates/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRate(int id, Rate rate)
+        {
+            if (id != rate.RateId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rates
+        [HttpPost]
+        public async Task<ActionResult<Rate>> PostRate(Rate rate)
+        {
+            _context.Rate.Add(rate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRate", new { id = rate.RateId }, rate);
+        }
+
+        // DELETE: api/Rates/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Rate>> DeleteRate(int id)
+        {
+            var rate = await _context.Rate.FindAsync(id);
+            if (rate == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rate.Remove(rate);
+            await _context.SaveChangesAsync();
+
+            return rate;
+        }
+
+        private bool RateExists(int id)
+        {
+            return _context.Rate.Any(e => e.RateId == id);
         }
     }
 }
